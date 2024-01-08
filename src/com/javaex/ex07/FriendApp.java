a98935e33bae05b8f09fe19150fd7d6de7a73650
rhs0402 <rhs0402@naver.com>
2024-01-08 18:04:23 +0900

    2024-01-08 + ex07

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -11,11 +11,23 @@ public class FriendApp {
         
         System.out.println("친구를 3명 등록해 주세요");
 
+        Friend f1 = new Friend();
+        friendArray[0] = f1;
+        Friend f2 = new Friend();
+        friendArray[1] = f2;
+        Friend f3 = new Friend();
+        friendArray[2] = f3;
         
         //for문 시작
         //친구정보 3명 입력 로직 --> 반복문 사용
         for(int i=0; i<friendArray.length; i++) {
-        	friendArray[i] = ;
+        	System.out.print("이름: ");
+        	friendArray[i].setName(sc.nextLine());
+        	System.out.print("핸드폰:");
+        	friendArray[i].setHp(sc.nextLine());
+        	System.out.print("학교:");
+        	friendArray[i].setSchool(sc.nextLine());
+        	System.out.println("-------------------------");
         }
         //for문 끝
         
@@ -23,7 +35,7 @@ public class FriendApp {
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
             //친구정보 출력 메소드 호출
-        	
+        	friendArray[i].showInfo();
         }
 
         sc.close();
