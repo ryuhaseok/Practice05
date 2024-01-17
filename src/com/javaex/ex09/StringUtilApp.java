58c62eb52d324773dd0eec90ede5cc8dd41d48ab
rhs0402 <rhs0402@naver.com>
2024-01-17 18:03:59 +0900

    0

--- src/com/javaex/ex09/StringUtilApp.java
+++ src/com/javaex/ex09/StringUtilApp.java
@@ -5,7 +5,7 @@ public class StringUtilApp {
     public static void main(String[] args) {
 
         String[] strArray = {"SuperMan", "BatMan", "SpiderMan"};
-        String resultStr = StringUtil.concatString(strArray);
+        String resultStr = StringUtil.concatString();
         
         System.out.println("결과 문자열:" + resultStr);
     }
--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -3,16 +3,52 @@ package com.javaex.ex10;
 public class Book {
     
 	//필드
+	int bookNo;
+	String title;
+	String author;
+	int stateCode;
 	
 	//생성자
 	public Book(int num, String title, String writer) {
-		
+		this.bookNo = num;
+		this.title = title;
+		this.author = writer;
 	}
+
 	
 	//메소드 g/s
+	public int getBookNo() {
+		return bookNo;
+	}
+	
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 	
 	//메소드 일반
-    
+    public void rent() {
+    	this.stateCode = 0;
+    	System.out.println(this.getTitle() + "이(가) 대여 됐습니다.");
+    }
+    public void print() {
+    	
+    }
     
     
     
